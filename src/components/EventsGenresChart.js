a0dbc000ee88fb733bd9c72f3f9337a539349047
Adrian Dever <ad.dever@yahoo.com>
2024-08-28 15:22:34 +0200

    Update: colors

--- src/components/EventsGenresChart.js
+++ src/components/EventsGenresChart.js
@@ -1,13 +1,15 @@
 import { useEffect, useState, useCallback, useMemo } from "react";
-import { ResponsiveContainer, PieChart, Pie } from "recharts";
+import { ResponsiveContainer, PieChart, Pie, Cell } from "recharts";
 
 const EventGenresChart = ({ events }) => {
   const genres = useMemo(
     () => ["React", "JavaScript", "Node", "jQuery", "Angular"],
     []
   );
-
   const [data, setData] = useState([]);
+
+  // Define a color for each genre
+  const colors = ["#8884d8", "#82ca9d", "#ffc658", "#ff8042", "#8dd1e1"];
 
   const getData = useCallback(() => {
     return genres.map((genre) => {
@@ -53,11 +55,14 @@ const EventGenresChart = ({ events }) => {
         <Pie
           data={data}
           dataKey="value"
-          fill="#8884d8"
           label={renderCustomizedLabel}
           labelLine={false}
           outerRadius={150}
-        />
+        >
+          {data.map((entry, index) => (
+            <Cell key={`cell-${index}`} fill={colors[index % colors.length]} />
+          ))}
+        </Pie>
       </PieChart>
     </ResponsiveContainer>
   );
